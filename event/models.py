8b2a71d8dbbb70f52e48973c506323fd4a92aedb
Fred High <fred.high@ndsu.edu>
2023-05-03 23:39:32 -0500

    Created models for apps

--- event/models.py
+++ event/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    day = models.ForeignKey("weekday.Week", on_delete=models.CASCADE)
+    start_time = models.IntegerField()
+    end_time = models.IntegerField()
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
