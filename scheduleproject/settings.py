01acca61e50099c00f1ab9be0a16e132c739581e
Fred High <fred.high@ndsu.edu>
2023-05-08 16:28:06 -0500

    Turned off debug

--- scheduleproject/settings.py
+++ scheduleproject/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0+35ou+0@+r#df*06kq^s9p0%msvk27*5p&0s9^*(hkq#)gvs8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
